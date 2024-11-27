adfa6dd54ba9c77ea4eed30ad703050242ec38d1
joshy <josh.ludahl@gmail.com>
2024-11-27 12:09:37 -0800

    (chore): Update with initial files

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,40 @@
-import type { Config } from "tailwindcss";
+import type {Config} from "tailwindcss";
+import {nextui} from "@nextui-org/react";
+
 
 export default {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
+    content: [
+        "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+        "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                background: "var(--background)",
+                foreground: "var(--foreground)",
+            },
+        },
     },
-  },
-  plugins: [],
+    plugins: [
+        nextui({
+            prefix: "nextui", // prefix for themes variables
+            addCommonColors: false, // override common colors (e.g. "blue", "green", "pink").
+            defaultTheme: "dark", // default theme from the themes object
+            defaultExtendTheme: "light", // default theme to extend on custom themes
+            layout: {}, // common layout tokens (applied to all themes)
+            themes: {
+                light: {
+                    layout: {}, // light theme layout tokens
+                    colors: {}, // light theme colors
+                },
+                dark: {
+                    layout: {}, // dark theme layout tokens
+                    colors: {}, // dark theme colors
+                },
+                // ... custom themes
+            },
+        })
+    ],
 } satisfies Config;
